a944ab48752e7140d995a7931bebd7fcdf5e97b9
lulinieva <evelynieva@hotmail.com>
2021-05-21 16:34:54 -0300

    se crea App.java y se realiza cambios en el package

---
+++ src/main/java/ar/com/ada/creditos/App.java
@@ -0,0 +1,17 @@
+package ar.com.ada.creditos;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+        ABM miABM = new ABM();
+
+        miABM.iniciar();
+
+    }
+}
