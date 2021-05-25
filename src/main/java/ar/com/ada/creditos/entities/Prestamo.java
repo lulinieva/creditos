ef9b9a8a28256945c90d769c8557cc111c469151
lulinieva <evelynieva@hotmail.com>
2021-05-25 17:22:28 -0300

    Creamos archivo Prestamo.java, agregamos los private y generamos los getters y setters.

---
+++ src/main/java/ar/com/ada/creditos/entities/Prestamo.java
@@ -0,0 +1,71 @@
+package ar.com.ada.creditos.entities;
+
+import java.util. *;
+import java.math. *;
+
+public class Prestamo {
+
+    private int prestamoId;
+
+    private Date fecha;
+
+    private BigDecimal importe;
+
+    private int cuotas;
+
+    private Date fechaAlta;
+
+    private Cliente cliente;
+
+    public int getPrestamoId() {
+        return prestamoId;
+    }
+
+    public void setPrestamoId(int prestamoId) {
+        this.prestamoId = prestamoId;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public BigDecimal getImporte() {
+        return importe;
+    }
+
+    public void setImporte(BigDecimal importe) {
+        this.importe = importe;
+    }
+
+    public int getCuotas() {
+        return cuotas;
+    }
+
+    public void setCuotas(int cuotas) {
+        this.cuotas = cuotas;
+    }
+
+    public Date getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(Date fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+
+    
+   
+}
